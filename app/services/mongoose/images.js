4d458f8b75e2d09d427e755951de293c9bb14ef7
unknown <dianraudhi56@gmail.com>
2023-01-25 18:58:37 +0700

    first commit

---
+++ app/services/mongoose/images.js
@@ -0,0 +1,33 @@
+const Images = require('../../api/v1/images/model');
+const {NotFoundError} = require('../../errors');
+
+
+// Cara kedua untuk generate image
+const generateUrlImage = async (req) => {
+  const result =  `uploads/${req.file.filename}`
+  
+  return result;
+};
+
+// Cara Pertama untuk generate image
+const createImages = async (req) => {
+  const result = await Images.create({
+    name: req.file
+      ? `uploads/${req.file.filename}`
+      : 'uploads/avatar/default.jpg',
+  });
+
+  return result;
+};
+
+// tambahkan function checking Image 
+const checkingImage = async (id) => {
+  const result = await Images.findOne({ _id: id });
+  console.log(result);
+
+  if (!result) throw new NotFoundError(`Tidak ada Gambar dengan id :  ${id}`);
+
+  return result;
+};
+// jangan lupa export checkingImage
+module.exports = { createImages, checkingImage };
